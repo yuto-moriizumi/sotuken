88c1bcdd923b838fd0cfdf835aa62bc2c51104e4
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-06-17 06:48:55 +0900

    モノラルマイクに対応

--- Streamer.py
+++ Streamer.py
@@ -15,6 +15,9 @@ DUMMY_BYTE_TYPE = np.float64
 
 class MixedSoundStreamClient(threading.Thread):
     USE_WAV = False
+    CHUNK = 1024  # 1度の送信で音声情報を何バイト送るか (なぜか指定数値の4倍量が送られる)
+    # → 512バイト/2バイト*8ビット→ 4倍量 になってると思われる
+    CHANNELS = 2
 
     def __init__(self, server_host, server_port, wav_filename):
         threading.Thread.__init__(self)
@@ -47,33 +50,36 @@ class MixedSoundStreamClient(threading.Thread):
         # オーディオプロパティ
         # TODO: プロパティをwavではなくマイクのみで設定したい(マイクがwavファイルに合わせられない時がある)
         FORMAT = pyaudio.paInt16
-        CHANNELS = wav_file.getnchannels() if wav_file != None else 1
-        RATE = wav_file.getframerate() if wav_file != None else 22050
+        if wav_file != None:
+            self.CHANNELS = wav_file.getnchannels()
+        RATE = wav_file.getframerate() if wav_file != None else 44100
         DUMMY_BITS_PER_NUMBER = 64
         # 何バイトのダミーバイトを先頭に含むか 2バイトで数字1つ送れる
         DUMMY_BYTES = 3*(DUMMY_BITS_PER_NUMBER//8)
-        CHUNK = 1024  # 1度の送信で音声情報を何バイト送るか (なぜか指定数値の4倍量が送られる)
-        # → 512バイト/2バイト*8ビット→ 4倍量 になってると思われる
 
         # マイクの入力ストリーム生成
         # デバイスを0番から順番に試す
         mic_stream = None
         print('Streamer initiated, creating mic stream')
-        for i in range(audio.get_device_count()):
-            try:
-                device_info = audio.get_device_info_by_index(i)
-                device_name: str = device_info["name"]
-                if "USB" in device_name or "Voice" in device_name:  # 名前に USB または Voice を含むデバイスならストリームを作成
-                    mic_stream = audio.open(format=FORMAT,
-                                            channels=CHANNELS,
-                                            rate=RATE,
-                                            input=True,
-                                            frames_per_buffer=CHUNK, input_device_index=i)
-                    print(
-                        f"mic stream created with {device_info}")
-                    break
-            except:
-                pass
+        # 予定出力チャンネルでのストリーム開設を目指すが、失敗したらチャンネル数を1減らす
+        mic_channels = 2
+        for channels in range(self.CHANNELS, 0, -1):
+            for i in range(audio.get_device_count()):
+                try:
+                    device_info = audio.get_device_info_by_index(i)
+                    device_name: str = device_info["name"]
+                    if "USB" in device_name or "Voice" in device_name:  # 名前に USB または Voice を含むデバイスならストリームを作成
+                        mic_stream = audio.open(format=FORMAT,
+                                                channels=channels,
+                                                rate=RATE,
+                                                input=True,
+                                                frames_per_buffer=self.CHUNK, input_device_index=i)
+                        print(
+                            f"mic stream created with {device_info}")
+                        mic_channels = channels
+                        break
+                except:
+                    pass
         if mic_stream == None:
             print("[WARN] creating mic_stream failed")
 
@@ -83,7 +89,7 @@ class MixedSoundStreamClient(threading.Thread):
 
             # サーバにオーディオプロパティを送信
             data = "{},{},{},{},{}".format(
-                FORMAT, CHANNELS, RATE, CHUNK, DUMMY_BYTES).encode('utf-8')
+                FORMAT, self.CHANNELS, RATE, self.CHUNK, DUMMY_BYTES).encode('utf-8')
             print(f"send:{data}")
             sock.send(data)
 
@@ -96,34 +102,34 @@ class MixedSoundStreamClient(threading.Thread):
             while True:
                 # 音楽ファイルからデータ読み込み
                 if wav_file != None:
-                    wav_data = wav_file.readframes(CHUNK)
+                    wav_data = wav_file.readframes(self.CHUNK)
                     # 音楽ファイルリピート再生処理
                     if wav_data == b'':
                         wav_file.rewind()
-                        wav_data = wav_file.readframes(CHUNK)
+                        wav_data = wav_file.readframes(self.CHUNK)
                 if mic_stream != None:  # マイクストリーム読み込み
-                    mic_data = mic_stream.read(CHUNK)
+                    mic_data = mic_stream.read(self.CHUNK)
                 # サーバに音データを送信
                 # ダミーの数値データ 数字1つで2バイト
                 # 今回チャンクから4バイト引いているので 2つまで送れるはず
-                # さて、なぜか送信するのはCHUNKの4倍量。サーバ側プログラムで対処。
+                # さて、なぜか送信するのはself.CHUNKの4倍量。サーバ側プログラムで対処。
                 # dummy = np.array(
                 #     [10*(i + 1) for i in range(DUMMY_BYTES//2)], np.int16)
                 dummy = np.array(
                     [lat, lon, alt], DUMMY_BYTE_TYPE)
                 if wav_file == None and mic_stream == None:  # どちらもない場合は空の音楽データを送信
                     data = self.mix_sound(
-                        CHANNELS, CHUNK, b"", 1)
+                        self.CHUNK, b"", 1, self.CHANNELS)
                 else:
                     if wav_file != None and mic_stream == None:  # マイクがなく、wavがある場合はwavをそのまま送信
                         data = self.mix_sound(
-                            CHANNELS, CHUNK, wav_data, 1)
+                            self.CHUNK, wav_data, 1, self.CHANNELS)
                     if wav_file == None and mic_stream != None:  # wavがなくマイクがある場合はマイクをそのまま送信
                         data = self.mix_sound(
-                            CHANNELS, CHUNK, mic_data, 1)
+                            self.CHUNK, mic_data, 1, mic_channels)
                     if wav_file != None and mic_stream != None:  # どちらもある場合はミックスして送信
                         data = self.mix_sound(
-                            CHANNELS, CHUNK, wav_data, 0.5, mic_data, 0.5)
+                            self.CHUNK, wav_data, 0.5, self.CHANNELS, mic_data, 0.5, mic_channels)
                     print(f"dymmy:{dummy} data:{data[0:8]}")
 
                 # data = np.append(dummy, data)
@@ -158,22 +164,33 @@ class MixedSoundStreamClient(threading.Thread):
         audio.terminate()
 
     # 2つの音データを1つの音データにミックス 1つしか渡されない場合は単にデコードする
-    def mix_sound(self,  channels, frames_per_buffer, data1, volume1, data2=None, volume2=0):
+    def mix_sound(self, frames_per_buffer, data1, volume1, channels1, data2=None, volume2=0, channels2=1):
         # 音量チェック
         if volume1 + volume2 > 1.0:
             return None
+        # 出力チャンネル数の決定
+        out_channels = max(channels1, channels2, self.CHANNELS)
+
         # デコード
         decoded_data1: np.ndarray = np.frombuffer(data1, np.int16).copy()
+        # データサイズの不足分を0埋め
+        decoded_data1.resize(channels1 * frames_per_buffer, refcheck=False)
+        if channels1 < out_channels:
+            decoded_data1 = self.mono2stereo(decoded_data1)
         if data2 != None:
             decoded_data2: np.ndarray = np.frombuffer(data2, np.int16).copy()
-        # データサイズの不足分を0埋め
-        decoded_data1.resize(channels * frames_per_buffer, refcheck=False)
-        if data2 != None:
-            decoded_data2.resize(channels * frames_per_buffer, refcheck=False)
-        # 音量調整 & エンコード
-        if data2 != None:
+            decoded_data2.resize(channels2 * frames_per_buffer, refcheck=False)
+            if channels2 < out_channels:
+                decoded_data2 = self.mono2stereo(decoded_data2)
             return (decoded_data1 * volume1 + decoded_data2 * volume2).astype(np.int16)
         return (decoded_data1 * volume1).astype(np.int16)
+
+    def mono2stereo(self, data: np.ndarray):
+        output_data = np.zeros((2, self.CHUNK))
+        output_data[0] = data
+        output_data[1] = data
+        output_data = np.reshape(output_data.T, (self.CHUNK * 2))
+        return output_data.astype(np.int16)
 
     def rungps(self, gps):  # GPSモジュールを読み、GPSオブジェクトを更新する
         import serial
