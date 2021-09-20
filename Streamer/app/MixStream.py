7b871475fa31c70bc8a1fd37b6071f442798ac07
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-09-20 15:55:51 +0900

    コード整理

--- Streamer/app/MixStream.py
+++ Streamer/app/MixStream.py
@@ -9,14 +9,9 @@ class MixStream(BytesStream):
         self.format_bit = max([s.format_bit for s in streams])
 
     def readNdarray(self, frames: int):
-        stream1 = self.streams[0]
-        stream2 = self.streams[1]
-
-        volume1 = 0.5
-        volume2 = 0.5
         volume = 1/len(self.streams)
 
-        # TODO: streamの長さが1の場合はただ返すだけ
+        # streamの長さが1の場合はただ返すだけ
         if len(self.streams) == 1:
             return np.frombuffer(
                 self.streams[0].readBytes(frames), self.streams[0].dtype)
@@ -27,7 +22,7 @@ class MixStream(BytesStream):
             decoded_data: np.ndarray = np.frombuffer(
                 stream.readBytes(frames), stream.dtype)
             # モノラルならステレオに変換
-            if stream1.channel < self.channel:
+            if stream.channel < self.channel:
                 decoded_data = self.mono2stereo(decoded_data, frames)
             # データサイズの不足分を0埋め
             decoded_data.resize(self.channel * frames, refcheck=False)
@@ -39,6 +34,9 @@ class MixStream(BytesStream):
         return data
 
     def readBytes(self, frames: int):
+        # streamの長さが1の場合はただ返すだけ
+        if len(self.streams) == 1:
+            return self.streams[0].readBytes(frames)
         return self.readNdarray(frames).tobytes()
 
     def mono2stereo(self, data: np.ndarray, frames: int):
