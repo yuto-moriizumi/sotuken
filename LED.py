2e447da29e295b501e3194dcd4367dbb3b0b3673
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2021-12-15 09:41:21 +0900

    Lチカ用プログラムを追加

---
+++ LED.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+while True:
+    GPIO.output(4, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(4, GPIO.LOW)
+    time.sleep(0.5)
