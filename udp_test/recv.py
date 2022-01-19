681068d1c56bc67a58e73e53bd4d7f1a46b7a354
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2022-01-19 21:52:55 +0900

    UDP通信のサンプル

---
+++ udp_test/recv.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST_NAME = ''
+PORT = 8080
+# ipv4を使うので、AF_INET
+# udp通信を使いたいので、SOCK_DGRAM
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# ブロードキャストするときは255.255.255.255と指定せずに空文字
+sock.bind((HOST_NAME, PORT))
+
+while True:
+    # データを待ち受け
+    rcv_data, addr = sock.recvfrom(1024)
+    print(f"receive data : [{rcv_data}]  from {addr}")
+
+sock.close()
